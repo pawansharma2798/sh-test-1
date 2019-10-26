4b9ac3ba386d97e154aa0f39a370f65fb5ca178b
pawansharma.200999 <pawansharma.200999@gmail.com>
2019-10-26 12:15:54 +0530

    testing

--- test.sh
+++ test.sh
@@ -1,2 +1,9 @@
-current_date=$(date)
-echo "Today is $current_date"
+MY_SHELL="zsh"
+
+if [ "$MY_SHELL" = "zs" ]
+then
+output = $(mkdir "Hello")
+else
+output = $(mkdir "Hi")
+fi
+echo "Output of command is $current_date"
