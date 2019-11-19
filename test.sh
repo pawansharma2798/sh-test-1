7ce47c8e3b34f936585395a1e4b6da575a22bca0
pawansharma2798 <57932725+pawansharma2798@users.noreply.github.com>
2019-11-19 10:44:48 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
 curl -X GET --header "Authorization: Basic cGF3YW4uc2hhcm1hQG5lb3NhbHBoYS5jb206UDEyMzQ1NkBq" "https://api.enterprise.apigee.com/v1/organizations/earthport/apis/CICD-PoC/deployments" >> output.json
+echo "Done"
